44ee573aed17d8b94fff4bf37e2f468d0023bd0f
p <pierrechabaud@live.fr>
2019-10-20 22:25:40 +0200

    [modify] main.cpp
    first test with sfml & images resizing

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -2,6 +2,7 @@
 
 // Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 void testSFML() {
     sf::Texture texture;
@@ -15,16 +16,43 @@ using namespace std;
 
 int main(int argc,char* argv[])
 {
-    /* Test le nombre d'argument dan sle terminal */
-    if ( argc != 2 )
+    /* Gestion de la commande en entrée */
+	if ( argc != 2 ) {
 		cout<<"usage: "<< argv[0] <<" <command>\n";
-	
-	//arg = std::cout << argv[1] << std::endl;
+	}
 	string entry = argv[1];
 	if (entry == "Hello") {
 		cout << "Hello world" << endl;
 	} else if (entry == "state") {
 		system("make code-coverage");
+	} else if (entry == "render") {
+
+		/* TESTS DE RENDU */
+		sf::RenderWindow renderWindow(sf::VideoMode(800, 550), "RobotIS");
+		sf::Event event;
+		sf::Texture texture;
+		texture.loadFromFile("../res/map.png");
+
+		sf::Sprite sprite(texture);
+
+		while (renderWindow.isOpen()){
+			while (renderWindow.pollEvent(event)){
+			if (event.type == sf::Event::EventType::Closed)
+				renderWindow.close();
+			}
+
+			renderWindow.clear();
+			renderWindow.draw(sprite);
+			renderWindow.display();
+		}
+
+
+
+
+
+
+
+
 	} else {
 		cout << "Fonction doesn't exist" << endl;
 	}
